ccb9b0273c561e160cb66dfcef4c2c2f1c60e216
Robin Mehlitz <robin.mehlitz@beuth-hochschule.de>
2017-07-18 13:10:12 +0200

    Add first working leaflet container

--- berlin-corner/imports/ui/body.js
+++ berlin-corner/imports/ui/body.js
@@ -3,7 +3,20 @@ import { Corners } from '../api/corners/corners.js';
 
 import  './corner.js';
 import './body.html';
+    
+Template.map.onRendered( function() {
+  L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
 
+  var map = L.map('mapId', {
+    doubleClickZoom: false
+  }).setView([49.25044, -123.137], 13);
+
+  L.tileLayer.provider('Stamen.Watercolor').addTo(map);
+});
+    
+   /* L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
+    var map = L.map('mapId');
+     L.tileLayer.provider('Stamen.Watercolor').addTo(map);*/
 
 Template.body.helpers({
   corners() {
